a4a8e9fb580749a2b5d8ea74bbd18540cf5ac1f7
PranjeetGoswami <prgoswami2020@gmail.com>
2024-12-05 22:07:14 +0530

    add to repo

--- src/components/displayalbum.jsx
+++ src/components/displayalbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData } from '../assets/assets';
+import { albumsData, assets } from '../assets/assets';
 
 const Displayalbum=()=> {
     
@@ -11,6 +11,27 @@ const Displayalbum=()=> {
     return (
         <>
             <Navbar/>   
+            <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+                <img className='w-48 rounded' src={albumData.image} alt="" />
+                <div className='flex flex-col'>
+                    <p>Playlist</p>
+                    <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+                    <h4>{albumData.desc}</h4>
+                    <p className='mt-1'>
+                        <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+                        <b>Spotify</b>
+                        • 1,326,451 likes
+                        • <b>50 Songs,</b>
+                        about 2 hr 30 min
+                    </p>
+                </div>
+            </div>
+            <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+                <p><b className='mr-4'>#</b>Title</p>
+                <p>Album</p>
+                <p className='hidden sm:block'>Date Added</p>
+                <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+            </div>
         </>
     )
 }
